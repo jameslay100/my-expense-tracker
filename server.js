a17bf666ab63e955f597c42900a6d2a22343c4c0
James Lay <jlayy25@gmail.com>
2024-04-07 19:48:13 +1000

    Add routes to add and list expense page

--- server.js
+++ server.js
@@ -1,8 +1,33 @@
 const http = require('http');
+const fs = require("fs");
 
 http.createServer(function (request, response) {
+    let fileName = "";
+    let myData = "";
     console.log("We got a request from: " + request.url);
-    response.write("Thank you for your request");
-    response.end();
+    // can use switch case here to route but i think u can do a seperate route file instead
+    switch (request.url) {
+        case "/":
+            fileName = "./views/index.html";
+            break;
+        case "/add-expense":
+            fileName = "./views/add-expense.html";
+            break;
+        case "/view-expenses":
+            fileName = "./views/view-expenses.html";
+            break;
+    }
+    if (fileName !== "") {
+        fs.readFile(fileName, function (err, data) {
+            response.write(data);
+            response.end();
+        });
+    }
+
+    // fs.readFile("./views/index.html", function (err, data) {
+    //     myData = data;
+    //     response.write(myData);
+    //     response.end();
+    // });
 })
-.listen(8080);
+    .listen(8080);
